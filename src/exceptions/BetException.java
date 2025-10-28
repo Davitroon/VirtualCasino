55cb21871f8003181acd541cad7c6dd14c79d083
Davitroon <deivitroon@gmail.com>
2025-10-28 22:16:57 +0100

    [Docs] Exceptions package Javadoc documentation
    * Improved and added Javadoc documentation on the entire Exceptions package

--- src/exceptions/BetException.java
+++ src/exceptions/BetException.java
@@ -1,15 +1,26 @@
 package exceptions;
 
 /**
- * Exception that will be thrown when a bet is not possible for different
- * reasons.
+ * Custom exception thrown when a bet cannot be placed due to various reasons.
+ * <p>
+ * This exception is used to signal errors related to invalid betting actions
+ * in the application, such as insufficient funds, invalid bet amount, or
+ * other game-specific constraints.
+ * </p>
  * 
- * @param message The error message.
+ * @author Davitroon
+ * @since 3.0
  */
 public class BetException extends Exception {
 
 	private static final long serialVersionUID = -4757589471438310285L;
 
+	/**
+	 * Constructs a new {@code BetException} with the specified detail message.
+	 * 
+	 * @param message the detail message explaining the reason for the exception
+	 * @since 3.0
+	 */
 	public BetException(String message) {
 		super(message);
 	}
--- src/exceptions/ExceptionMessage.java
+++ src/exceptions/ExceptionMessage.java
@@ -1,34 +1,46 @@
 package exceptions;
 
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
 
 /**
- * Class that will display an error message in case of exceptions and close the
- * program. It is the first class to be instantiated in the program, so it can
- * be called before the model has been created.
+ * Utility class to handle exceptions by displaying a user-friendly error
+ * message and logging technical details to the console.
+ * <p>
+ * This class is intended to be instantiated first in the program so that it
+ * can handle exceptions even before the model or other components are fully
+ * initialized.
+ * </p>
+ * 
+ * @author Davitroon
+ * @since 3.0
  */
 public class ExceptionMessage {
 
-	/**
-	 * Method to display an error message when an exception occurs.
-	 * 
-	 * @param e           Exception
-	 * @param userMessage Error message to display to the user
-	 * @since 3.0
-	 */
-	public void showError(Exception e, String userMessage) {
-		JOptionPane.showMessageDialog(null, userMessage, "DB Connection Error", JOptionPane.ERROR_MESSAGE);
-		System.out.println("(" + e.getClass().getSimpleName() + ") " + e.getMessage());
-		e.printStackTrace();
+    /**
+     * Displays an error message dialog to the user and logs detailed exception
+     * information to the console.
+     * <p>
+     * If the exception is an instance of {@link SQLException}, the SQL state and
+     * error code are also printed.
+     * <p>
+     * After showing the error, the program will terminate to prevent continued
+     * execution with an unstable state.
+     * 
+     * @param e           The exception that occurred.
+     * @param userMessage The message to display to the user explaining the error.
+     * @since 3.0
+     */
+    public void showError(Exception e, String userMessage) {
+        JOptionPane.showMessageDialog(null, userMessage, "DB Connection Error", JOptionPane.ERROR_MESSAGE);
+        System.out.println("(" + e.getClass().getSimpleName() + ") " + e.getMessage());
+        e.printStackTrace();
 
-		if (e instanceof SQLException sqlEx) {
-			System.out.println("SQLState: " + sqlEx.getSQLState() + " - Error Code: " + sqlEx.getErrorCode());
-		}
+        if (e instanceof SQLException sqlEx) {
+            System.out.println("SQLState: " + sqlEx.getSQLState() + " - Error Code: " + sqlEx.getErrorCode());
+        }
 
-		// Faced with an error, I prefer to close the program so the user cannot use the
-		// application until it has been fixed.
-		System.exit(1);
-	}
+        // Terminate the application after a critical error
+        System.exit(1);
+    }
 }
